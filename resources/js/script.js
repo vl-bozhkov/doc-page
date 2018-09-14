c780f461e5edb7733037755cdf23b27d26f4d529
Vladimir Bozhkov <vl.bozhkov@gmail.com>
2018-09-14 10:01:16 +0300

    fixed bugs with responsiveness

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,50 +1,4 @@
 $(document).ready(function() {
-  /* Navigation scroll */
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') ==
-          this.pathname.replace(/^\//, '') &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate(
-            {
-              // scrollTop: target.offset().top
-              scrollTop: target.offset().top - $('nav').height()
-            },
-            1000,
-            function() {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(':focus')) {
-                // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              }
-            }
-          );
-        }
-      }
-    });
-
   $('.js--nav-icon').click(function() {
     var nav = $('.js--mobile-nav');
     var icon = $('.js--nav-icon i');
@@ -60,4 +14,8 @@ $(document).ready(function() {
       icon.removeClass('ion-md-close');
     }
   });
+});
+
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000
 });
